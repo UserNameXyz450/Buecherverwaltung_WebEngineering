e72a760c6f5ac49ea390064fb1747b80e94aeae9
person123789 <bertalam.lu@gmail.com>
2025-06-14 14:24:24 +0200

    HTML/CSS und Routing für/zu Register, Login und Home (bei Home nicht fertig)

--- Buecherverwaltung/src/app/app.component.ts
+++ Buecherverwaltung/src/app/app.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavbarComponent} from './navbar/navbar.component';
+import {HomeComponent} from './home/home.component';
+
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {ProfileComponent} from './profile/profile.component';
+import {ArticleComponent} from './article/article.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Buecherverwaltung/src/app/app.routes.ts
+++ Buecherverwaltung/src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent} from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import {RegisterComponent } from './register/register.component';
+import {NgModule} from '@angular/core';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {path: 'navbar', component: NavbarComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
