529588235de857bde2c2076b7f981907920b4cfb
person123789 <bertalam.lu@gmail.com>
2025-06-15 20:35:02 +0200

    HTML/CSS und bisschen Typescript für Profil-Seite, die grauen divs sind Platzhalter

--- Buecherverwaltung/src/app/app.component.ts
+++ Buecherverwaltung/src/app/app.component.ts
@@ -11,7 +11,7 @@ import {ArticleComponent} from './article/article.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Buecherverwaltung/src/app/profile/profile.component.ts
+++ Buecherverwaltung/src/app/profile/profile.component.ts
@@ -7,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  imageURL = "https://i.pravatar.cc/#100#"
+  name: string = "Name";
+  private number: number = Number(Math.random()*10);
+  changePicture(): void {
+    this.number = Number(Math.random()*10);
+    this.imageURL = `https://i.pravatar.cc/#${this.number}#`
+    console.log(this.imageURL);
+  }
 }
